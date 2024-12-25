79f980634d334b806efd2a61bb9123b7e8784933
Oleksii-Op <oleksii.oprish@gmail.com>
2024-12-26 01:03:57 +0200

    Middleware ip filtration

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -2,20 +2,22 @@ import uvicorn
 
 import logging
 
-from starlette.responses import JSONResponse
+from middleware_related.check_ip import check_ip_middleware
 
 from common_logger.logger_config import configure_logger
 from core.config import settings
 
 from api import router as api_router
 from create_fastapi_app import create_app
-from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 
 logger = logging.getLogger(__name__)
+
+from before_start_up.check_env_file import find_env
+from before_start_up.check_python_version import check_version
 
 
 main_app = create_app(
@@ -30,33 +32,6 @@ main_app.include_router(
 @main_app.get("/index")
 async def index_page():
     return {"message": "Hello, user!"}
-
-
-async def check_ip_middleware(
-    request: Request,
-    call_next,
-) -> JSONResponse:
-    try:
-        client_ip = request.client.host
-        logging.info(f"Request URL: {request.url}")
-        logging.info(f"Request Method: {request.method}")
-        logging.info(f"Client IP: {client_ip}")
-
-        if request.url.path == "/metrics":
-            if client_ip != "10.10.103.5":
-                logging.warning(f"Forbidden access attempt from IP: {client_ip}")
-                return JSONResponse(
-                    status_code=403,
-                    content={"detail": "Forbidden"},
-                )
-
-        return await call_next(request)
-
-    except Exception as e:
-        logging.error(f"Unexpected error occurred: {str(e)}")
-        return JSONResponse(
-            status_code=500, content={"detail": "Internal Server Error"}
-        )
 
 
 main_app.add_middleware(
@@ -78,13 +53,15 @@ Instrumentator().instrument(main_app).expose(main_app)
 
 if __name__ == "__main__":
     configure_logger(level=logging.INFO)
+    check_version()
+    find_env(".env")
     logger.info("Starting FastAPI application")
     uvicorn.run(
         "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
         # set reload=False if to be run in docker container
-        reload=False,
+        reload=True,
         log_config="log_conf.yaml",
     )
     logger.info("FastAPI application stopped")
