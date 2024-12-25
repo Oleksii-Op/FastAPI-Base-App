413cb461fa3792aca4182495846de486a29fa3ac
Oleksii-Op <oleksii.oprish@gmail.com>
2024-12-26 01:08:03 +0200

    bash script for testing backend /index for metrics testing

--- index_page_loader.sh
+++ index_page_loader.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-# This script simulates users GET query for "http://site.local:443/index" end-point.
-
-# It is used to get/test prometheus and grafana metrics
+# This script simulates asynchronous GET queries to the specified endpoint.
+# It is used to test Prometheus and Grafana metrics.
 
 # DO NOT forget to chmod +x to run the script!!!
 
 # Number of queries
 NUM_REQUESTS=10000
 
-# URL for the query
-URL="http://site.local:443/index"
 
-# Query header
+read -p "Enter url for load test 'https://localhost/index' -> " URL
+#URL="https://192.168.1.73/index"
+
+
 HEADER="accept: application/json"
 
-# Loop
-for ((i=1; i<=NUM_REQUESTS; i++))
-do
-  curl -X 'GET' "$URL" -H "$HEADER"
-  echo "Query #$i done"
+# Maximum number of parallel requests
+MAX_PARALLEL=50
+
+# Function to perform a single query
+make_request() {
+  curl --insecure -X 'GET' "$URL" -H "$HEADER" &> /dev/null
+  echo "Query #$1 done"
+}
+
+# Counter for active background jobs
+active_jobs=0
+
+
+for ((i=1; i<=NUM_REQUESTS; i++)); do
+  make_request "$i" &
+  ((active_jobs++))
+
+  # Limit the number of parallel jobs
+  if ((active_jobs >= MAX_PARALLEL)); then
+    wait -n  # Wait for at least one background job to finish
+    ((active_jobs--))
+  fi
 done
+
+wait
+
+echo "All $NUM_REQUESTS queries completed."
