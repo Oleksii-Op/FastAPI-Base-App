5f82fb5b8c20567625ee652c6392f1e6bddf3558
Oleksii-Op <oleksii.oprish@gmail.com>
2024-12-28 12:33:42 +0200

    changed check_ip.py

--- fastapi-application/middleware_related/check_ip.py
+++ fastapi-application/middleware_related/check_ip.py
@@ -9,24 +9,26 @@ async def check_ip_middleware(
     request: Request,
     call_next,
 ) -> JSONResponse:
-    try:
-        client_ip = request.client.host
-        logger.info(f"Request URL: {request.url}")
-        logger.info(f"Request Method: {request.method}")
-        logger.info(f"Client IP: {client_ip}")
+    # try:
+    client_ip = request.client.host
+    logger.info(f"Request URL: {request.url}")
+    logger.info(f"Request Method: {request.method}")
+    logger.info(f"Client IP: {client_ip}")
 
-        if request.url.path == "/metrics":
-            if client_ip != "10.10.103.5":
-                logger.warning(f"Forbidden access attempt from IP: {client_ip}")
-                return JSONResponse(
-                    status_code=403,
-                    content={"detail": "Forbidden"},
-                )
+    if request.url.path == "/metrics":
+        if client_ip != "10.10.103.5":
+            logger.warning(f"Forbidden access attempt from IP: {client_ip}")
+            return JSONResponse(
+                status_code=403,
+                content={"detail": "Forbidden"},
+            )
 
-        return await call_next(request)
+    return await call_next(request)
 
-    except Exception as e:
-        logger.error(f"Unexpected error occurred: {str(e)}")
-        return JSONResponse(
-            status_code=500, content={"detail": "Internal Server Error"}
-        )
+
+#
+# except Exception as e:
+#     logger.error(f"Unexpected error occurred: {str(e)}")
+#     return JSONResponse(
+#         status_code=500, content={"detail": "Internal Server Error"}
+#     )
